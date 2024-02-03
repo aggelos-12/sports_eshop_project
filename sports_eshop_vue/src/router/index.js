4fc1094c689c76b26e3af56af08677f3e4b62570
KOIS <flinkie2652@gmail.com>
2024-02-03 23:42:08 +0200

    signup commit

--- sports_eshop_vue/src/router/index.js
+++ sports_eshop_vue/src/router/index.js
@@ -2,12 +2,24 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import Product from '../views/Product.vue'
 import Cart from '../views/Cart.vue'
+import SignUp from '../views/SignUp.vue'
+import LogIn from '../views/LogIn.vue'
 const routes = [
   {
     path: '/',
     name: 'home',
     component: HomeView
   },
+  {
+    path: '/sign-up',
+    name: 'SignUp',
+    component: SignUp
+  },
+{
+    path: '/log-in',
+    name: 'LogIn',
+    component: LogIn
+},
   {
     path: '/about',
     name: 'about',
