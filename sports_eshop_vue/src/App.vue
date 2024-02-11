2e7e5522d875662542f3c3902b7daa78762a5c4f
Aggelos <angelosfekos@gmail.com>
2024-02-11 16:27:32 +0200

    version 1.0

--- sports_eshop_vue/src/App.vue
+++ sports_eshop_vue/src/App.vue
@@ -2,41 +2,73 @@
   <div id="wrapper">
     <nav class="navbar is-dark">
       <div class="navbar-brand">
+        <router-link to="/" class="navbar-item"><strong>Project</strong></router-link>
 
-        <router-link to="/"  class="navbar-item"><strong>Sports Eshop</strong></router-link>
+        <a class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbar-menu" @click="showMobileMenu = !showMobileMenu">
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
       </div>
 
-      <div class="navbar-menu" id="navbar-menu">
+      <div class="navbar-menu" id="navbar-menu" :class="{'is-active': showMobileMenu}">
+
+        <div class="navbar-start">
+          <div class="navbar-item">
+            <form method="get" action="/search">
+              <div class="field has-addons">
+                <div class="control">
+                  <input type="text" class="input" placeholder="search..." name="query">
+                </div>
+
+                <div class="control">
+                  <button class="button is-sucess">
+                    <span class="icon"><i class="fas fa-search"></i></span>
+                  </button>
+
+                </div>
+
+              </div>
+            </form>
+          </div>
+        </div>
         <div class="navbar-end">
           <router-link to="/summer" class="navbar-item">Summer</router-link>
           <router-link to="/winter" class="navbar-item">Winter</router-link>
 
           <div class="navbar-item">
             <div class="buttons">
-              <router-link to="/log-in" class="button is-light">Log in</router-link>
-              <router-link to="/sign-up" class="button is-light">Sign up</router-link>
+              <router-link to="/log-in" class="button is-light">Login</router-link>
               <router-link to="/cart" class="button is-success">
                 <span class="icon"><i class="fas fa-shopping-cart"></i></span>
                 <span>Cart({{ cartTotalLength }})</span>
+
               </router-link>
+
             </div>
+
           </div>
         </div>
       </div>
+
     </nav>
 
+    <div class="is-loading-bar has-text-centered" :class="{ 'is-loading': $store.state.isLoading }">
+      <div class="lds-dual-ring">
+
+      </div>
+    </div>
     <section class="section">
       <router-view/>
     </section>
 
     <footer class="footer">
-      <p class="has-text-centered">Copyrights (c) 2024</p>
+      <p class="has-text-centered">Sports Eshop 2024</p>
     </footer>
+
   </div>
 
-
 </template>
-
 
 
 <script>
