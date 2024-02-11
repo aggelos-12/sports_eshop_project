011501185f2750590a7ed12b00617ce6bdb22318
Aggelos <angelosfekos@gmail.com>
2024-02-11 16:39:23 +0200

    version 1.0

--- sports_eshop_vue/src/views/Product.vue
+++ sports_eshop_vue/src/views/Product.vue
@@ -3,23 +3,28 @@
         <div class="columns is-multiline">
             <div class="column is-9">
                 <figure class="image mb-6">
-                    <img v-bind:src="product.get_image">
+                    <img :src="product.get_image">
                 </figure>
 
-                <h1 class="title">{{ product.name }}</h1>
-            </div>
-            <div class="column is-3">
-                <h2 class="subtitle">Information</h2>
+                <h1 class="title">{{ product.name  }}</h1>
+                <p>{{ product.description }}</p>
 
+            </div>
+
+            <div class="collumn is-3">
+                <h2 class="subtitle">Information</h2>
                 <p><strong>Price: </strong>${{ product.price }}</p>
-                <div class="field has-addons mt-6">
+
+                <div class="filed has-addons mt-6">
                     <div class="control">
                         <input type="number" class="input" min="1" v-model="quantity">
                     </div>
+
                     <div class="control">
                         <a class="button is-dark" @click="addToCart">Add to cart</a>
                     </div>
                 </div>
+
             </div>
         </div>
     </div>
@@ -29,39 +34,35 @@
 <script>
 import axios from 'axios'
 import { toast } from 'bulma-toast'
-
 export default {
     name: 'Product',
-    data(){
+    data() {
         return{
             product: {},
             quantity: 1
-
         }
     },
-    mounted(){
 
+    mounted() {
         this.getProduct()
     },
+
     methods:{
-        getProduct(){
+        async getProduct() {
+            this.$store.commit('setIsLoading', true)
+
             const category_slug = this.$route.params.category_slug
             const product_slug = this.$route.params.product_slug
 
-            axios
-                .get(`/api/v1/products/${category_slug}/${product_slug}`)
-                .then(response=> {
-                    this.product =response.data
-
-                    document.title = this.product.name + ' | SportsHub'
-                })
-                .catch(error =>{
-                    console.log(error)
-                })
+           await axios.get(`/api/v1/products/${category_slug}/${product_slug}`)
+            .then(response => { this.product = response.data
+                                document.title = this.product.name +  ' | Sports Eshop'})
+            .catch(error => { console.log(error) })
+            this.$store.commit('setIsLoading', false)
         },
-        addToCart(){
-            console.log('addToCart')
-            if(isNaN(this.quantity) || this.quantity < 1){
+
+        addToCart () {
+            if (isNaN(this.quantity) || this.quantity < 1){
                 this.quantity = 1
             }
 
@@ -69,20 +70,17 @@ export default {
                 product: this.product,
                 quantity: this.quantity
             }
-            this.$store.commit('addToCart',item)
-
+            this.$store.commit('addToCart', item)
             toast({
                 message: 'The product was added to the cart',
-                type:'is-success',
-                dismissible:true,
-                pauseOnHover:true,
+                type: 'is-success',
+                dismissible: true,
+                pauseOnHover: true,
                 duration: 2000,
-                position: 'bottom-right'
+                position: 'bottom-right',
             })
-
-
-
         }
     }
+
 }
 </script>
