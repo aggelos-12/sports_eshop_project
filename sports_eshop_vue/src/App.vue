b089c6b5c61e3a968b9d3377e47b9cbea24d0270
Aggelos <angelosfekos@gmail.com>
2024-02-11 16:10:54 +0200

    version 1.0

--- sports_eshop_vue/src/App.vue
+++ sports_eshop_vue/src/App.vue
@@ -40,31 +40,39 @@
 
 
 <script>
-
-
-export default {
+import axios from 'axios'
+export default{
   data() {
     return {
-      cart:{
-        items: []
+      showMobileMenu: false,
+      cart: {
+        items:[]
       }
     }
   },
-  beforeCreate(){
+  beforeCreate() {
     this.$store.commit('initializeStore')
+
+    const token = this.$store.state.token
+
+    if (token){
+      axios.defaults.headers.common['Authorization'] = "Token " + token
+    }
+    else{
+      axios.defaults.headers.common['Authorization'] = ""
+    }
+
   },
   mounted(){
-    this.cart = this.$store.state.cart
-
-
+    this.cart =  this.$store.state.cart
   },
+
   computed: {
-    cartTotalLength(){
-      let totalLength= 0
+    cartTotalLength() {
+      let totalLength  = 0
 
-      for (let i=0; i<this.cart.items.length; i++){
+      for(let i =0;i < this.cart.items.length;i++){
         totalLength += this.cart.items[i].quantity
-
       }
 
       return totalLength
@@ -73,10 +81,42 @@ export default {
 }
 </script>
 
-
 <style lang="scss">
 @import '../node_modules/bulma';
+.lds-dual-ring {
+  display: inline-block;
+  width: 80px;
+  height: 80px;
+}
+.lds-dual-ring:after {
+  content: " ";
+  display: block;
+  width: 64px;
+  height: 64px;
+  margin: 8px;
+  border-radius: 50%;
+  border: 6px solid #ccc;
+  border-color: #ccc transparent #ccc transparent;
+  animation: lds-dual-ring 1.2s linear infinite;
+}
+@keyframes lds-dual-ring {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
 
+.is-loading-bar {
+  height: 0;
+  overflow: hidden;
 
+  -webkit-transition: all 0.3s;
+  transition: all 0.3s;
 
+  &.is-loading {
+    height: 80px;
+  }
+}
 </style>
